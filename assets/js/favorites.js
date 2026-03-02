3190b9d0f0fdd7f6aa4fa324f00070da881cd1ce
EdwinOcampo28 <235344181+EdwinOcampo28@users.noreply.github.com>
2026-03-01 19:26:44 -0500

    Delete.js

--- assets/js/favorites.js
+++
@@ -1,5 +0,0 @@
-function guardarFavorito(apod) {
-    let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-    favoritos.push(apod);
-    localStorage.setItem("favoritos", JSON.stringify(favoritos));
-}
